c3e957cb90d82e3a495477b8da9d36f350704843
Taras Rudyi <trudyi@rebbix.com>
2016-03-29 01:09:58 +0300

    Optionals refactoring + additional task

--- 01-playground/DiaryPlayground.playground/Contents.swift
+++ 01-playground/DiaryPlayground.playground/Contents.swift
@@ -2,14 +2,20 @@
 
 import Foundation
 
-class DiaryRecord {
+class DiaryRecord: CustomStringConvertible {
     let createdDate: NSDate
     var name: String?
     var text: String?
     var tags: [String]
     
-    init(name: String? = nil, text: String? = nil, tags: [String] = [String]()) {
-        self.createdDate = NSDate.init()
+    var description: String { return fullDescription() }
+    
+    init(createdDate: NSDate? = nil, name: String? = nil, text: String? = nil, tags: [String] = [String]()) {
+        if let createdDateUnwrapped = createdDate {
+            self.createdDate = createdDateUnwrapped
+        } else {
+            self.createdDate = NSDate()
+        }
         self.name = name
         self.text = text
         self.tags = tags
@@ -17,22 +23,65 @@ class DiaryRecord {
     
     func fullDescription() -> String {
         let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "dd MMMM YYYY"
         dateFormatter.locale = NSLocale(localeIdentifier: "uk_UK")
         
-        return "\(dateFormatter.stringFromDate(createdDate))" +
-            (name != nil ? "\n\(name!)" : "") +
-            (tags.count > 0 ? "\n[\(tags.joinWithSeparator("] ["))]" : "") +
-            (text != nil ? "\n\(text!)" : "") +
-            "\n"
+        let calendar = NSCalendar.currentCalendar()
+        let thisWeek = calendar.component(NSCalendarUnit.WeekOfYear, fromDate: NSDate())
+        let createdWeek = calendar.component(NSCalendarUnit.WeekOfYear, fromDate: self.createdDate)
+        
+        var description:String
+        if calendar.isDateInToday(self.createdDate)  {
+            dateFormatter.dateFormat = "HH:mm"
+            description = "\(dateFormatter.stringFromDate(createdDate))"
+        } else if calendar.isDateInYesterday(self.createdDate) {
+            description = "Вчора"
+        } else if createdWeek == thisWeek  {
+            dateFormatter.dateFormat = "EEEE"
+            description = "\(dateFormatter.stringFromDate(createdDate).capitalizedString)"
+        } else {
+            dateFormatter.dateFormat = "dd MMMM YYYY"
+            description = "\(dateFormatter.stringFromDate(createdDate))"
+        }
+        
+        if let name = self.name {
+            description = "\(description)\n\(name)"
+        }
+        if self.tags.count > 0 {
+            description = "\(description)\n[\(self.tags.joinWithSeparator("] ["))]"
+        }
+        if let text = self.text {
+            description = "\(description)\n\(text)"
+        }
+        description = "\(description)\n"
+        
+        return description
     }
 }
 
-var emptyRecord = DiaryRecord()
-print(emptyRecord.fullDescription())
+struct DatePeriods {
+    static let oneHour:Double = 3600
+    static let oneDay:Double  = 24 * DatePeriods.oneHour
+    static let oneWeek:Double = 7 * DatePeriods.oneDay
+    
+    static let now          = NSDate()
+    static let oneHourAgo   = DatePeriods.now.dateByAddingTimeInterval(-DatePeriods.oneHour)
+    static let yesterday    = DatePeriods.now.dateByAddingTimeInterval(-DatePeriods.oneDay)
+    static let threeDaysAgo = DatePeriods.now.dateByAddingTimeInterval(-3 * DatePeriods.oneDay)
+    static let oneWeekAgo   = DatePeriods.now.dateByAddingTimeInterval(-DatePeriods.oneWeek)
+}
 
-var completeRecord = DiaryRecord(name: "Сніданок", text:"П’ю каву, зимно і сумно, бо вже осінь.", tags: ["сумно", "дощ", "кава"])
-print(completeRecord.fullDescription())
+var emptyWeekRecord = DiaryRecord(createdDate: DatePeriods.threeDaysAgo)
+var yesterdayRecord = DiaryRecord(createdDate: DatePeriods.yesterday, text: "Вчив Swift.")
+var nowRecord       = DiaryRecord(name: "Зараз", text: "П’ю каву, пташки співають, бо вже весна!", tags: ["весна", "сонечко", "кава"])
+var weekAgoRecord   = DiaryRecord(createdDate: DatePeriods.oneWeekAgo, name: "Вечеря")
 
-var partialRecord = DiaryRecord(name: "Вечеря")
-print(partialRecord.fullDescription())
+
+// додаткове завдання
+
+let records = [weekAgoRecord, nowRecord, emptyWeekRecord, yesterdayRecord]
+
+for record in records.sort({ (firstRecord: DiaryRecord, secondRecord: DiaryRecord) -> Bool in
+    return firstRecord.createdDate.compare(secondRecord.createdDate) == NSComparisonResult.OrderedAscending
+}) {
+    print(record)
+}
