135bcc4b949827d908d478a57b5aab7ee71b7f38
Taras Rudyi <trudyi@rebbix.com>
2016-04-16 15:23:08 +0300

    Add Events tab

---
+++ 06-diary-app/Diary/Diary/EventView.swift
@@ -0,0 +1,21 @@
+//
+//  EventView.swift
+//  Diary
+//
+//  Created by Taras Rudyi on 4/16/16.
+//  Copyright © 2016 Taras Rudyi. All rights reserved.
+//
+
+import UIKit
+
+class EventView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
