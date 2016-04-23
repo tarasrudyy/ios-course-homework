6c74228b7dd9434b650c5b8f44fab7fbc13cdc48
Taras Rudyi <trudyi@rebbix.com>
2016-04-23 17:40:41 +0300

    Extract event tree view out controller

--- 07-diary-app/Diary/Diary/EventsViewController.swift
+++ 07-diary-app/Diary/Diary/EventsViewController.swift
@@ -10,89 +10,24 @@ import UIKit
 
 class EventsViewController: UIViewController {
 
-    // MARK: Properties
-    var diary:Diary?
-    
-    var displayedRecords: [DiaryRecord] {
-        if let diary = diary {
-            var sortedRecords = diary.records.sort({ (firstRecord: DiaryRecord, secondRecord: DiaryRecord) -> Bool in
-                return firstRecord.createdDate.compare(secondRecord.createdDate) == .OrderedAscending
-            })
-            
-            if isTimelineMode {
-                var i = 0
-                while i < sortedRecords.count - 1 {
-                    let record = sortedRecords[i]
-                    let nextRecord = sortedRecords[i + 1]
-                    let calendar = NSCalendar.currentCalendar()
-                    let dayNumber = calendar.ordinalityOfUnit(.Day, inUnit: .Era, forDate: record.createdDate)
-                    let nextDayNumber = calendar.ordinalityOfUnit(.Day, inUnit: .Era, forDate: nextRecord.createdDate)
-                    
-                    if 2...7 ~= nextDayNumber - dayNumber {
-                        let emptyRecord = DiaryRecord()
-                        if let date = calendar.dateByAddingUnit(.Day, value: 1, toDate: record.createdDate, options: .MatchStrictly) {
-                            emptyRecord.createdDate = date
-                        }
-                        emptyRecord.weather = Weather.None
-                        sortedRecords.insert(emptyRecord, atIndex: i + 1)
-                    }
-                    i = i + 1
-                }
+    var diary:Diary? {
+        didSet {
+            if let treeView = self.view as? EventTreeView {
+                treeView.records = diary?.records
             }
-            
-            return sortedRecords
         }
-        return [DiaryRecord]()
     }
-    
-    var isTimelineMode = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func viewWillAppear(animated: Bool) {
-        reloadView()
+        if let treeView = self.view as? EventTreeView {
+            treeView.reloadView()
+        }
     }
     
-    func reloadView() {
-        for subview in self.view.subviews {
-            subview.removeFromSuperview()
-        }
-        
-        let offsetX:CGFloat = 30
-        var nextY:CGFloat = 0
-        let records = displayedRecords
-        for record in records {
-            let eventView = EventView()
-            
-            if record == records.first {
-                eventView.isFirst = true
-            } else if record == records.last {
-                eventView.isLast = true
-            }
-            
-            if isTimelineMode && record.weather != .None && record != records.first {
-                if let index = records.indexOf(record) {
-                    let prevRecord = records[index - 1]
-                    let calendar = NSCalendar.currentCalendar()
-                    if calendar.isDate(record.createdDate, inSameDayAsDate: prevRecord.createdDate) {
-                        eventView.hasDate = false
-                    }
-                }
-            }
-            
-            eventView.record = record
-            eventView.frame.origin = CGPointMake(offsetX, nextY)
-            nextY += eventView.frame.height
-            self.view.addSubview(eventView)
-        }
-        
-        if let scrollView = self.view as? UIScrollView {
-            scrollView.contentSize.height = nextY
-        }
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -100,8 +35,9 @@ class EventsViewController: UIViewController {
     
     @IBAction func onViewModeChanged(sender: AnyObject) {
         if let segmentedControl = sender as? UISegmentedControl {
-            isTimelineMode = segmentedControl.selectedSegmentIndex == 1
-            reloadView()
+            if let treeView = self.view as? EventTreeView {
+                treeView.isTimelineMode = segmentedControl.selectedSegmentIndex == 1
+            }
         }
     }
     
